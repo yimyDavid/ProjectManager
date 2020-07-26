c01b478cf49c98be0438d9c3a88c21f1296d3b0c
Yimy <yimysol@gmail.com>
2020-07-25 23:59:47 -0400

    Added date picker for transactions

--- app/src/main/java/com/ctmy/expensemanager/new_transaction.java
+++ app/src/main/java/com/ctmy/expensemanager/new_transaction.java
@@ -2,14 +2,17 @@ package com.ctmy.expensemanager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class new_transaction extends AppCompatActivity {
+    TextView tvTransDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,22 @@ public class new_transaction extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        tvTransDate = (TextView) findViewById(R.id.tv_due_date);
+
+        tvTransDate.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               showDatePickerDiaglog(v);
+                                           }
+                                       }
+
+        );
+    }
+
+    private void showDatePickerDiaglog(View v){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+
     }
 }
--- app/src/main/java/com/ctmy/expensemanager/project_transactions.java
+++ app/src/main/java/com/ctmy/expensemanager/project_transactions.java
@@ -2,13 +2,17 @@ package com.ctmy.expensemanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 public class project_transactions extends AppCompatActivity {
 
     private Project project;
+    TextView addTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,16 @@ public class project_transactions extends AppCompatActivity {
         setContentView(R.layout.activity_project_transactions);
         Toolbar toolbar = findViewById(R.id.toolbarTrans);
         setSupportActionBar(toolbar);
+
+        addTransaction = (TextView) findViewById(R.id.btAdd);
+        addTransaction.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View v) {
+                                                  Intent transIntent = 
+                                              }
+                                          }
+
+        );
 
         // Add back button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -31,4 +45,5 @@ public class project_transactions extends AppCompatActivity {
         this.project = project;
         setTitle(getResources().getText(R.string.proj_trans_acitivity) + " " + project.getProjectName());
     }
+
 }

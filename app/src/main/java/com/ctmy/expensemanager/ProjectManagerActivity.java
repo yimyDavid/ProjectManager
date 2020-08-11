ec0dfe0fad10bbf4a07e01b47e1ecba3f602f1f7
Yimy <yimysol@gmail.com>
2020-08-10 22:45:26 -0400

    Implemented logout

--- app/src/main/java/com/ctmy/expensemanager/ProjectManagerActivity.java
+++ app/src/main/java/com/ctmy/expensemanager/ProjectManagerActivity.java
@@ -2,13 +2,21 @@ package com.ctmy.expensemanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProjectManagerActivity extends AppCompatActivity {
@@ -39,6 +47,32 @@ public class ProjectManagerActivity extends AppCompatActivity {
         });*/
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.project_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.logout_menu:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d("Logout", "User Logged Out");
+                                FirebaseUtil.attachListener();
+                            }
+                        });
+                FirebaseUtil.detachListener();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void initializeDisplayContent(){
         RecyclerView rvProjects = (RecyclerView) findViewById(R.id.projects);

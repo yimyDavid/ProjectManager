c922996ccb0224628c7c2cecfe7e21a02c3a6e93
Yimy <yimysol@gmail.com>
2020-08-11 23:49:27 -0400

    Check is user is administrator

--- app/src/main/java/com/ctmy/expensemanager/FirebaseUtil.java
+++ app/src/main/java/com/ctmy/expensemanager/FirebaseUtil.java
@@ -1,11 +1,16 @@
 package com.ctmy.expensemanager;
 
 import android.app.Activity;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,6 +25,7 @@ public class FirebaseUtil {
     public static FirebaseAuth mFirebaseAuth;
     public static FirebaseAuth.AuthStateListener mAuthListener;
     public static ArrayList<Project> mProjects;
+    public static boolean isAdmin;
 
     private static final int RC_SIGN_IN = 123;
     private static Activity caller;
@@ -38,12 +44,48 @@ public class FirebaseUtil {
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                     if(firebaseAuth.getCurrentUser() == null){
                         FirebaseUtil.signIn();
+                    }else{
+                        String userId = firebaseAuth.getUid();
+                        checkAdmin(userId);
                     }
                 }
             };
         }
         mProjects = new ArrayList<>();
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+
+    private static void checkAdmin(String userId) {
+        FirebaseUtil.isAdmin = false;
+        DatabaseReference ref = mFirebaseDatabase.getReference().child("administrator").child(userId);
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                FirebaseUtil.isAdmin = true;
+                Log.d("Admin", "You are an administrator");
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        ref.addChildEventListener(listener);
     }
 
     private static void signIn(){

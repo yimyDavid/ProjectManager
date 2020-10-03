e609a22595e116cccdf6c7d15a437394351db6b2
Yimy <yimysol@gmail.com>
2020-10-02 23:51:30 -0400

    Method stub to handle dates

---
+++ app/src/main/java/com/ctmy/expensemanager/DateUtil.java
@@ -0,0 +1,16 @@
+package com.ctmy.expensemanager;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static String epochToString(Long dateMillis){
+        return null;
+    }
+
+    public static Long stringToEpoch(String stringDate){
+        return 1L;
+    }
+}

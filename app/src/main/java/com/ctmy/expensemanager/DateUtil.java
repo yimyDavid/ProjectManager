4fad42f9af3f88e44931ed90c12c0513fe4ce527
Yimy <yimysol@gmail.com>
2020-10-04 23:13:28 -0400

    Just get epoch time and save like long

--- app/src/main/java/com/ctmy/expensemanager/DateUtil.java
+++ app/src/main/java/com/ctmy/expensemanager/DateUtil.java
@@ -1,5 +1,8 @@
 package com.ctmy.expensemanager;
 
+import com.google.firebase.database.ServerValue;
+
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,23 +11,29 @@ import java.util.TimeZone;
 
 public class DateUtil {
 
-    public static String epochToString(Long dateMillis){
-        return String.valueOf(dateMillis);
+    public static Long getEpochTimeStamp(){
+        return System.currentTimeMillis();
     }
 
-    public static Long stringToEpoch(String stringDate){
+    /*public static Long stringToEpoch(String stringDate){
         return Long.valueOf(stringDate);
-    }
+    }*/
 
+    public static String epochToDateString(Long epoch, String timezone){
+        Date date = new Date(System.currentTimeMillis());
+
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        format.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
+        String formatted = format.format(date);
+        System.out.println(formatted);
+        format.setTimeZone(TimeZone.getTimeZone("Australia/Sydney"));
+        formatted = format.format(date);
+        System.out.println(formatted);
+
+        return formatted;
+    }
     /* The result is to display it in the views*/
     public static String convertUTCtoCurrentTime(Long dateMillis){
-        Date date = new Date(dateMillis);
 
-        SimpleDateFormat df = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss a", Locale.ENGLISH);
-        df.setTimeZone(TimeZone.getTimeZone("UTC"));
-        Date datey = df.parse(date.toString());
-        df.setTimeZone(TimeZone.getDefault());
-        String fomrattedDate = df.format(datey);
-        return fomrattedDate;
     }
 }

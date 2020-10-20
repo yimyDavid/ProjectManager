61da89c88fdaef2f4d4b1ccce4d40a34df6f1679
Yimy <yimysol@gmail.com>
2020-10-19 23:15:29 -0400

    Update totals when loading the transactions

--- app/src/main/java/com/ctmy/expensemanager/ProjectTransaction.java
+++ app/src/main/java/com/ctmy/expensemanager/ProjectTransaction.java
@@ -4,18 +4,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
@@ -31,9 +36,13 @@ public class ProjectTransaction extends AppCompatActivity  implements ValuesFrom
     TextView tvTotalIncomes;
 
     String mProjectTitle="";
+    String mProjectId="";
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
     // final String PROJECT_NAME = "com.ctmy.expensemanager.PROJECT_NAME";
+
+    Double expenses = 0.0 ;
+    Double incomes = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,10 +75,13 @@ public class ProjectTransaction extends AppCompatActivity  implements ValuesFrom
 
         );
 
+        initializeDisplayTransactions();
+
         if(project == null){
             project = new Project();
             SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(PROJECT_NAME, 0);
             mProjectTitle = sharedPreferences.getString("project_name", "");
+            mProjectId = sharedPreferences.getString("project_id", "");
         }else{
             mProjectTitle = project.getProjectName();
             SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(PROJECT_NAME, 0);
@@ -79,18 +91,11 @@ public class ProjectTransaction extends AppCompatActivity  implements ValuesFrom
             editor.putString("project_id", project.getProjectId());
             editor.commit();
 
-            /* update total for main project */
-            FirebaseUtil.openFbReference("projects", null);
-            mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
-            mDatabaseReference = FirebaseUtil.mDatabaseReference;
-            mDatabaseReference.child(project.getProjectId()).child("totalIncomes").setValue(Double.valueOf(tvTotalIncomes.getText().toString()));
-            mDatabaseReference.child(project.getProjectId()).child("totalExpenses").setValue(Double.valueOf(tvTotalExpenses.getText().toString()));
+
 
         }
 
         setTitle(getResources().getText(R.string.proj_trans_acitivity) + " " + mProjectTitle);
-
-        initializeDisplayTransactions();
     }
 
     @Override
@@ -110,9 +115,42 @@ public class ProjectTransaction extends AppCompatActivity  implements ValuesFrom
 
     @Override
     public void getTotalExpenses(HashMap<String, Double> totalAmount) {
-        Double expenses = totalAmount.get("egresos");
-        Double incomes =  totalAmount.get("ingresos");
+        expenses = totalAmount.get("egresos");
+        incomes =  totalAmount.get("ingresos");
         tvTotalExpenses.setText(Double.valueOf(expenses).toString());
         tvTotalIncomes.setText(Double.valueOf(incomes).toString());
+
+        updateProjectTotals();
+    }
+
+    public void updateProjectTotals(){
+
+        Log.d("value", tvTotalIncomes.getText().toString() + incomes);
+        // Not working
+        /* update total for main project */
+        FirebaseUtil.openFbReference("projects", null);
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+
+        mDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            String projectId;
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(project.getProjectId() != null) {
+                    projectId = project.getProjectId();
+                }else{
+                    projectId = mProjectId;
+                }
+                mDatabaseReference.child(projectId).child("totalIncomes").setValue(incomes);
+                mDatabaseReference.child(projectId).child("totalExpenses").setValue(expenses);
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
